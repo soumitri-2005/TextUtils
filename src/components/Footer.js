2b6c5b6e7729c1e76e6c8cd53d81a3ee730702a3
Soumitri Mishra <149056652+soumitri-2005@users.noreply.github.com>
2024-10-24 12:21:15 +0530

    final update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,8 @@ export default function Footer(props) {
     <div>
       <footer class={`bg-${props.mode} text-center text-lg-start`} style={{backgroundColor: props.mode === "dark" ? "black": "white", color: props.mode === "dark" ? "white": "black", paddingTop: "20px"}}>
         <div class="container text-center">
-            <h5 class="text-uppercase" style={{padding: "20px"}}>about</h5>
-            <p style={{paddingBottom: "20px"}}>TextUtils is your go-to tool for handling all your text-related tasks. Need to count words or characters in a document? TextUtils has you covered. It helps you streamline your text by removing extra spaces and supports various text case conversions, including uppercase, lowercase, and capitalizing the first letter of each word. It also features a dark mode for a more comfortable viewing experience. With a user-friendly interface, TextUtils makes it easy to copy text or clear the text box when starting afresh. Stay efficient and enhance your text management with TextUtils!</p>
+            <h5 class="text-uppercase" style={{paddingTop: "20px"}}>about</h5>
+            <p style={{padding: "20px", textAlign: "justify"}}>TextUtils is your all-in-one tool for text-related tasks. Count words/characters, remove extra spaces, and convert text cases (uppercase, lowercase, capitalize). It offers a dark mode and a user-friendly interface for easy text management. Stay efficient with TextUtils!</p>
         </div>
         <div class="text-center p-3" style={{borderTop: "1px solid gray"}}>
             Made with <i class="fa-solid fa-heart"></i> by Soumitri.
